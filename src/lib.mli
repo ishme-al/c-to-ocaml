63f2f0b1e5c63241a625e9cc4db52a5a1724c9c3
root <theoleehusky@gmail.com>
2023-11-17 21:48:24 +0000

    some function definitions

--- src/lib.mli
+++ src/lib.mli
@@ -1,1 +1,8 @@
+open Clang
+
 val parse : Clang.Ast.translation_unit -> Out_channel.t -> unit
+
+val visit_stmt : Ast.stmt -> Out_channel.t -> unit
+val visit_function_decl : Ast.function_decl -> Out_channel.t -> unit
+val visit_decl : Ast.decl -> Out_channel.t -> unit
+(* many more of this ... *)
