1c391b16b7b7fbcd22f6bb57eacc5def9aa64693
Aprina Wang <awang100@jhu.edu>
2023-12-17 21:50:04 -0500

    fix dune file

--- tests/source/print.c
+++ tests/source/print.c
@@ -5,8 +5,8 @@ void foo() {
 }
 
 int main() {
-    int x = 1;
+    int x[3] = {1, 2, 3};
     float y = 2.0;
-    printf("Hello, world %d %.2f!\n", x, y);
+    printf("Hello, world %d %.2f!\n", x[1], y);
     return 0;
 }
