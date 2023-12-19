457b177bf458bf5449dfc9e572682d2d2a08880a
root <theoleehusky@gmail.com>
2023-12-19 17:25:40 +0000

    fixed .gitignore for testing

---
+++ tests/actual/int.ml
@@ -0,0 +1,16 @@
+[@@@ocaml.warning "-26"]
+[@@@ocaml.warning "-27"]
+[@@@ocaml.warning "-32"]
+[@@@ocaml.warning "-69"]
+
+open Core
+
+let rec set_at_index (lst : 'a list) (index : int) (value : 'a) : 'a list =
+  match lst with
+  | [] -> failwith "Index out of bounds"
+  | hd :: tl ->
+      if index = 0 then value :: tl else hd :: set_at_index tl (index - 1) value
+
+let () =
+  let a = 2 in
+  exit 0
---
+++ tests/actual/nestedloops.ml
@@ -0,0 +1,57 @@
+[@@@ocaml.warning "-26"]
+[@@@ocaml.warning "-27"]
+[@@@ocaml.warning "-32"]
+[@@@ocaml.warning "-69"]
+
+open Core
+
+let rec set_at_index (lst : 'a list) (index : int) (value : 'a) : 'a list =
+  match lst with
+  | [] -> failwith "Index out of bounds"
+  | hd :: tl ->
+      if index = 0 then value :: tl else hd :: set_at_index tl (index - 1) value
+
+let foo () : int =
+  let x : int = 0 in
+  let i : int = 0 in
+  let rec for_foo i x =
+    match Int.( < ) i 5 with
+    | false -> x
+    | true ->
+        let j : int = 0 in
+        let rec for_for_foo j x =
+          match Int.( < ) j 3 with
+          | false -> x
+          | true ->
+              let x = x + 1 in
+              let j = j + 1 in
+              for_for_foo j x
+        in
+        let x = for_for_foo j x in
+        let i = i + 1 in
+        for_foo i x
+  in
+  let x = for_foo i x in
+  let i : int = 0 in
+  let rec for_foo i x =
+    match Int.( < ) i 3 with
+    | false -> x
+    | true ->
+        let rec while_for_foo x =
+          match Int.( > ) x 3 with
+          | false -> x
+          | true ->
+              let x = x - 1 in
+              while_for_foo x
+        in
+        let x = while_for_foo x in
+        let i = i + 1 in
+        for_foo i x
+  in
+  let x = for_foo i x in
+  x
+
+let () =
+  let x : int = foo () in
+  printf "%d" x;
+  exit 0
